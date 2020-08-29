06ec5c5a8e3b4655e1039421a249db2a7fd8a15b
Melkov Denis <d.a.melkov@gmail.com>
2020-08-29 15:55:04 +0300

    add menu

--- app/src/main/java/com/astra/melkovhw122/MainActivity.java
+++ app/src/main/java/com/astra/melkovhw122/MainActivity.java
@@ -1,8 +1,10 @@
 package com.astra.melkovhw122;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
